9e7f884a7b95c806eddd935981d4db25341b2f33
Leighanne Lyvers <lflyvers@icloud.com>
2024-11-08 16:38:15 -0500

    More updates to the documentation layout.

--- src/pages/documentation.py
+++ src/pages/documentation.py
@@ -7,38 +7,87 @@ import dash_mantine_components as dmc
 register_page(__name__)
 
 documentation_page_container = Container(
-                id="documentation-page-container",
-                children=[
-                    Text(
-                        "Documentation Page", 
-                        id="documentation-page-title",
-                        style={"fontSize": "22px", "fontWeight": "bold", "marginBottom": "10px", "color": "#34495e"}
+    id="documentation-page-container",
+    children=[
+        Text(
+            "Documentation Page", 
+            id="documentation-page-title",
+            style={"fontSize": "22px", "fontWeight": "bold", "marginBottom": "10px", "color": "#34495e"}
+        ),
+        dmc.Accordion(
+            chevronPosition="left",
+            variant="contained",
+            radius="sm",
+            children=[
+                dmc.AccordionItem(
+                [
+                    dmc.AccordionControl(
+                        dmc.Group(
+                            [
+                                dmc.Text("Data Gathering"),
+                            ]
+                        )
                     ),
-                   dmc.Accordion(
-                        chevronPosition="left",
-                        variant="contained",
-                        children=[
-                            dmc.AccordionItem(
-                                [
-                                    dmc.AccordionControl(
-                                        dmc.Group(
-                                        [
-                                            dmc.Text("Header Title"),
-                                        ]
-                                        )
-                                    ),
-                                    dmc.AccordionPanel(
-                                        dmc.Text("This is the body content of the dropdown.", size="sm")
-                                    ),
-                                ],
-                                value="header",
-                            )
-                        ],
-                        multiple=True
+                    dmc.AccordionPanel(
+                        dmc.Text("Here is where we will detail how and where the data was gathered from.", size="sm"),
                     ),
                 ],
-                style={"padding": "20px"}
-            )
+                value="dataGathering",
+                ),
+
+                dmc.AccordionItem(
+                [
+                    dmc.AccordionControl(
+                        dmc.Group(
+                            [
+                                dmc.Text("Data Processing"),
+                            ]
+                        )
+                    ),
+                    dmc.AccordionPanel(
+                        dmc.Text("Here is where we will detail how the data was processed.", size="sm")
+                    ),
+                ],
+                value="dataProcessing",
+                ),
+
+                dmc.AccordionItem(
+                [
+                    dmc.AccordionControl(
+                        dmc.Group(
+                            [
+                                dmc.Text("Understanding the Data/How to use this website"),
+                            ]
+                        )
+                    ),
+                    dmc.AccordionPanel(
+                        dmc.Text("Here is where we will detail important things to understand the data/how to use this website. This will help those who maybe aren't directly in the mental health field. ", size="sm")
+                    ),
+                ],
+                value="usingWebsite",
+                ),
+
+                dmc.AccordionItem(
+                [
+                    dmc.AccordionControl(
+                        dmc.Group(
+                            [
+                                dmc.Text("Mental Health Resources"),
+                            ]
+                        )
+                    ),
+                    dmc.AccordionPanel(
+                        dmc.Text("Here is where we will include some important mental health resources", size="sm")
+                    ),
+                ],
+                value="mentalHealth",
+                )
+            ],
+            multiple=True
+        ),
+    ],
+    style={"padding": "20px"}
+)
 
 layout = html.Div(
         id="documentation-page-content",
