c6fe0bf3a1b9f5a6cf268b8dfcd2854298a16a19
Leighanne Lyvers <lflyvers@icloud.com>
2024-11-08 11:26:32 -0500

    Added in a basic accordion dropdown as a placeholder for future documentation.

--- src/pages/documentation.py
+++ src/pages/documentation.py
@@ -1,7 +1,11 @@
 from dash import html, register_page
-from dash_mantine_components import Container, Text
+from dash_mantine_components import Container, AccordionControl, AccordionPanel, Group, Avatar, Text
+import dash_mantine_components as dmc
+
+
 
 register_page(__name__)
+
 documentation_page_container = Container(
                 id="documentation-page-container",
                 children=[
@@ -9,13 +13,33 @@ documentation_page_container = Container(
                         "Documentation Page", 
                         id="documentation-page-title",
                         style={"fontSize": "22px", "fontWeight": "bold", "marginBottom": "10px", "color": "#34495e"}
-                        # could use drop down sections that show header and then when clicked show the rest of the info
-                        # or could utilize a list of links to separate pages
                     ),
-                   
+                   dmc.Accordion(
+                        chevronPosition="left",
+                        variant="contained",
+                        children=[
+                            dmc.AccordionItem(
+                                [
+                                    dmc.AccordionControl(
+                                        dmc.Group(
+                                        [
+                                            dmc.Text("Header Title"),
+                                        ]
+                                        )
+                                    ),
+                                    dmc.AccordionPanel(
+                                        dmc.Text("This is the body content of the dropdown.", size="sm")
+                                    ),
+                                ],
+                                value="header",
+                            )
+                        ],
+                        multiple=True
+                    ),
                 ],
                 style={"padding": "20px"}
             )
+
 layout = html.Div(
         id="documentation-page-content",
         children=[
