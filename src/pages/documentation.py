13003e00536bfff7da0d24512e62d63fc7c2d86f
ejme232 <ejme232@uky.edu>
2024-11-21 15:14:07 -0500

    Data citation in documenation page

--- src/pages/documentation.py
+++ src/pages/documentation.py
@@ -4,6 +4,8 @@ import dash_mantine_components as dmc
 
 register_page(__name__)
 
+data_gathering_string="Our data was gathered by the Substance Abuse and Mental Health Services Administration (SAMHSA) for the 2022 National Survey on Drug Use and Health (NSDUH).\nAdditional information about this database can be found at https://www.samhsa.gov/data/data-we-collect/nsduh/datafiles?data_collection=1124&year=2015, and information about how this data was collected can be viewed at https://www.samhsa.gov/data/sites/default/files/reports/rpt44477/2022-nsduh-mrb-dcfr.pdf."
+
 documentation_page_container = Container(
     id="documentation-page-container",
     children=[
@@ -11,6 +13,31 @@ documentation_page_container = Container(
             "Documentation Page", 
             id="documentation-page-title",
             style={"fontSize": "22px", "fontWeight": "bold", "marginBottom": "10px", "color": "#34495e"}
+        ),
+        Text(
+            "Author: Substance Abuse and Mental Health Services Administration (SAMHSA) Center for Behavioral Health Statistics and Quality", 
+            id="documentation-citation",
+            style={"fontSize": "18px", "marginBottom": "10px"}
+        ),
+        Text(
+            "Date of publication: October 25, 2023", 
+            id="documentation-citation",
+            style={"fontSize": "18px", "marginBottom": "10px"}
+        ),
+        Text(
+            "Title: 2022 National Survey on Drug Use and Health (NSDUH)", 
+            id="documentation-citation",
+            style={"fontSize": "18px", "marginBottom": "10px"}
+        ),
+        Text(
+            "Publisher: SAMHSA", 
+            id="documentation-citation",
+            style={"fontSize": "18px", "marginBottom": "10px"}
+        ),
+        dmc.Anchor(
+            "URL: https://www.samhsa.gov/data/data-we-collect/nsduh/datafiles?year=2022&data_collection=1186", 
+            href="https://www.samhsa.gov/data/data-we-collect/nsduh/datafiles?year=2022&data_collection=1186",
+            style={"fontSize": "18px", "marginBottom": "10px"}
         ),
         dmc.Accordion(
             chevronPosition="left",
@@ -27,7 +54,7 @@ documentation_page_container = Container(
                         )
                     ),
                     dmc.AccordionPanel(
-                        dmc.Text("Here is where we will detail how and where the data was gathered from.", size="sm"),
+                        dmc.Text(data_gathering_string, size="sm"),
                     ),
                 ],
                 value="dataGathering",
